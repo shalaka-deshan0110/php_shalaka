52d9abd06bba4eb8faed651df10386c281806872
Shalaka Deshan Silva <salu86er@gmail.com>
2022-01-05 10:24:52 +0000

    Add navigation link in the navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('sale-representatives.index') }}">{{ __('Sales Team') }}</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
